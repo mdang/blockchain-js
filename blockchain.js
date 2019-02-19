87da8c820e6ed3a8c73b4ac51470471006964da4
Mike Dang <mike.dang@generalassemb.ly>
2019-02-19 17:53:47 -0600

    Coded through example

--- blockchain.js
+++ blockchain.js
@@ -8,6 +8,61 @@ class Blockchain {
   }
 
   createGenesisBlock() {
-    return new Block(new Date().getTime(), [], '0')
+    return new Block(Date.now(), [], '0')
   }
+
+  getLatestBlock() {
+    return this.chain[this.chain.length - 1]
+  }
+
+  createTransaction(transaction) {
+    this.pendingTransactions.push(transaction)
+  }
+
+  minePendingTransactions(miningRewardAddress) {
+    let block = new Block(Date.now(), this.pendingTransactions, this.getLatestBlock().hash)
+    block.mineBlock(this.difficulty)
+
+    this.chain.push(block)
+
+    this.pendingTransactions = [
+      new Transaction(null, miningRewardAddress, this.miningReward)
+    ]
+  }
+
+  isChainValid() {
+    for (let i = 1, x = this.chain.length; i < x; i++) {
+      const currentBlock = this.chain[i]
+      const previousBlock = this.chain[i - 1]
+
+      if (currentBlock.hash !== currentBlock.calculateHash()) {
+        return false
+      }
+
+      if (currentBlock.previousHash !== previousBlock.hash) {
+        return false
+      }
+    }
+
+    return true
+  }
+
+  getBalanceOfAddress(address) {
+    let balance = 0
+
+    for (const block of this.chain) {
+      for (const transaction of block.transactions) {
+        if (transaction.fromAddress === address) {
+          balance -= transaction.amount
+        }
+
+        if (transaction.toAddress === address) {
+          balance += transaction.amount
+        }
+      }
+    }
+    
+    return balance
+  }
+
 }
